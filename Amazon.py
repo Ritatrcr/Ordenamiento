29a1f0817baf0fa91c640bbf6e6d0fd44736b5ab
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-04 10:46:00 -0500

    hola mario

---
+++ Amazon.py
@@ -0,0 +1,22 @@
+print("Somos Amazon")
+#hola mario
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        temp = arr[i]
+        j = i
+        while j > 0 and arr[j - 1] > temp:
+            arr[j] = arr[j - 1]
+            j -= 1
+        arr[j] = temp
+    return arr
+
+def selection_sort(arr):    
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
