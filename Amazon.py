78405fb19f2405b2884074cf15e42373a551325e
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-04 11:40:59 -0500

    usar funciones y arrays organizados

--- Amazon.py
+++ Amazon.py
@@ -1,5 +1,15 @@
-print("Somos Amazon")
-#hola mario
+print("Bienvenido a Amazon Bodega Management System.")
+print("Optimizando la organización de nuestros productos en la bodega...\n")
+
+productos_bodega = [
+    [102],   # Ropa
+    [78, 56, 91, 34, 72],    # Escolar
+    [55, 88, 39,],   # Tecnologia
+    [99, 44, 32, 6]     # Hogar
+]
+
+
+
 def insertion_sort(arr):
     n = len(arr)
     for i in range(1, n):
@@ -10,16 +20,23 @@ def insertion_sort(arr):
             j -= 1
         arr[j] = temp
     return arr
-#holaaaa
+
 def selection_sort(arr):    
     n = len(arr)
     for i in range(n):
         min_index = i
         for j in range(i + 1, n):
-            if arr[j] < arr[min_index]:
+            if len(arr[j]) < len(arr[min_index]):
                 min_index = j
         arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
 
+productos_bodega_organizados = []
 
-#ya lo cree
+for i in range(len(productos_bodega)):
+    productos_bodega_organizados.append(insertion_sort(productos_bodega[i]))
+
+
+print (selection_sort(productos_bodega_organizados))
+
+
