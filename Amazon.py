321f4ef2be7f281c1997fb0eb64be0dca58a39a1
luisramu@unisabana.edu.co <luisramu@unisabana.edu.co>
2024-03-04 13:35:58 -0500

    añadir informacion a los array imprimir arrays ordenados y bodega ordenada

--- Amazon.py
+++ Amazon.py
@@ -1,14 +1,14 @@
 print("Bienvenido a Amazon Bodega Management System.")
 print("Optimizando la organización de nuestros productos en la bodega...\n")
 
+ropa = [102, 684, 618, 354, 3848, 983, 483, 278, 596, 742, 105, 327],
+escolar = [78, 56, 91, 34, 72, 87, 694, 684, 753, 2384, 1630, 1998, 1024, 1201, 1895],
+tecnologia = [55, 88, 39, 843, 2080, 2749, 2985, 3665, 3957, 2096, 3345],
+hogar = [99, 44, 32, 6, 3015, 3547, 3892, 3402, 3867, 3689, 3471]
+
 productos_bodega = [
-    [102],   # Ropa
-    [78, 56, 91, 34, 72],    # Escolar
-    [55, 88, 39,],   # Tecnologia
-    [99, 44, 32, 6]     # Hogar
+    ropa, escolar,tecnologia, hogar, 
 ]
-
-
 
 def insertion_sort(arr):
     n = len(arr)
@@ -36,7 +36,9 @@ productos_bodega_organizados = []
 for i in range(len(productos_bodega)):
     productos_bodega_organizados.append(insertion_sort(productos_bodega[i]))
 
+bodega_organizada = selection_sort(productos_bodega_organizados)
 
-print (selection_sort(productos_bodega_organizados))
+for i in range(len(bodega_organizada)):
+    print(bodega_organizada[i])
 
 
