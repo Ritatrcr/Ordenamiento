2f866564dfc518d5a6032b07e602490774590be7
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-05 08:47:43 -0500

    arreglo funciones random

--- Amazon.py
+++ Amazon.py
@@ -1,15 +1,30 @@
 import random
+
 print("Bienvenido a Amazon Bodega Management System.")
 print("Optimizando la organización de nuestros productos y precios en la bodega...\n")
 
-ropa = [random.randint(0, 100) for _ in range(random.randint(1, 10))],
-escolar = [random.randint(0, 100) for _ in range(random.randint(1, 10))],
-tecnologia = [random.randint(0, 100) for _ in range(random.randint(1, 10))],
-hogar =[random.randint(0, 100) for _ in range(random.randint(1, 10))]
+ropa = [random.randint(0, 100) for _ in range(random.randint(1, 10))]
+escolar = [random.randint(0, 100) for _ in range(random.randint(1, 10))]
+tecnologia = [random.randint(0, 100) for _ in range(random.randint(1, 10))]
+hogar = [random.randint(0, 100) for _ in range(random.randint(1, 10))]
 
-productos_bodega = [
-    ropa, escolar,tecnologia, hogar, 
-]
+print("BODEGA ORIGINAL: ")
+print("Ropa", ropa)
+print("Escolar", escolar)
+print("Tecnología", tecnologia)
+print("Hogar", hogar)
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if len(arr[j]) < len(arr[min_index]):
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
 
 def insertion_sort(arr):
     n = len(arr)
@@ -22,33 +37,16 @@ def insertion_sort(arr):
         arr[j] = temp
     return arr
 
-def selection_sort(arr):    
-    n = len(arr)
-    for i in range(n):
-        min_index = i
-        for j in range(i + 1, n):
-            if len(arr[j]) < len(arr[min_index]):
-                min_index = j
-        arr[i], arr[min_index] = arr[min_index], arr[i]
-    return arr
-
-productos_bodega_organizados = []
-
-for i in range(len(productos_bodega)):
-    productos_bodega_organizados.append(insertion_sort(productos_bodega[i]))
-
-bodega_organizada1 = selection_sort(productos_bodega_organizados)
-
-print ("BODEGA ORIGINAL: ")
-print("Ropa",ropa)
-print("Escolar",escolar)
-print("Tecnología",tecnologia)
-print("Hogar",hogar)
+productos_bodega = [
+    insertion_sort(ropa), insertion_sort(escolar), insertion_sort(tecnologia), insertion_sort(hogar)
+]
 
 
+#organiza los productos de la bodega por tamaños
+bodega_organizada = selection_sort(productos_bodega)
 
 print ("\nBODEGA ORGANIZADA POR PRECIOS Y TAMAÑOS: ")
-for i in range(len(bodega_organizada1)):
-    print(bodega_organizada1[i])
+for i in range(len(bodega_organizada)):
+    print(bodega_organizada[i])
 
 
