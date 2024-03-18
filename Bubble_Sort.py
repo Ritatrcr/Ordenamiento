004b2cf76b366cc0751e29790d68283c043e0943
Rita Trindade da Cruz <ritatrcr@unisabana.edu.co>
2024-03-18 07:17:45 -0500

    Cambios en Bubble y creacion de Selection

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,9 +1,53 @@
+#Todos tienen la misma notación O(n^2) pero el mejor caso es O(n) y el peor caso es O(n^2)
+
 # Método Burbuja
 
 def bubble_sort(lista):
-    n = len(lista)
-    for i in range(n):
-        for j in range(0, n-i-1):
+    for i in range(len(lista)):
+        for j in range(0, len(lista)-i-1):
             if lista[j] > lista[j+1]:
                 lista[j], lista[j+1] = lista[j+1], lista[j]
     return lista
+
+#genera lista random
+import random
+lista = random.sample(range(0, 100), 10)
+print("Lista original: ", lista)
+print("Lista ordenada: ", bubble_sort(lista))
+
+# Método Burbuja Mejorado
+def bubble_sort_mejorado(lista):
+    i=0
+    control=True
+    while i<len(lista) and control:
+        control=False
+        for j in range(0, len(lista)-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                control=True
+        i+=1
+    return lista
+
+#Mejor Método Burbuja (Coctel)
+
+def bubble_sort_mejor(lista):
+    izquierda = 0
+    derecha = len(lista) - 1
+    control = True
+    while (izquierda < derecha and control):
+        control = False
+        for i in range(izquierda, derecha):
+            if (lista[i] > lista[i + 1]):
+                lista[i], lista[i + 1] = lista[i + 1], lista[i]
+                control = True
+        derecha -= 1
+        for i in range(derecha, izquierda, -1):
+            if (lista[i] < lista[i - 1]):
+                lista[i], lista[i - 1] = lista[i - 1], lista[i]
+                control = True
+        izquierda += 1
+
+    return lista
+
+
+    
---
+++ Selection.py
@@ -0,0 +1,10 @@
+#Método por Selección
+
+def selection_sort(lista):
+    for i in range(len(lista)):
+        minimo = i
+        for j in range(i+1, len(lista)):
+            if lista[j] < lista[minimo]:
+                minimo = j
+        lista[i], lista[minimo] = lista[minimo], lista[i]
+    return lista
